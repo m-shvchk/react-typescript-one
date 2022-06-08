1fbb1cf5e7d3fb70c042cd83bb1246d7fb64c32e
m-shvchk <thirdinthequeue@gmail.com>
2022-06-08 08:15:11 +0300

    list of items and props types

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Todos from './components/Todos'
+import Todo from './models/todo'
 
 function App() {
+  const todos =[
+    new Todo('todo#1'),
+    new Todo('todo#2')
+  ]
   return (
     <div>
-      <Todos items = {['item 1', 'item 2']}/>
+      <Todos items = {todos}/>
     </div>
   );
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import Todo from '../models/todo'
+import TodoItem from './TodoItem'
 
-const Todos: React.FC<{ items: string[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
   // type: react func. component, in the <> brackets - types of custom props
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item}>{item}</li>
+        <TodoItem key={item.id} text={item.text}/>
       ))}
     </ul>
   );
